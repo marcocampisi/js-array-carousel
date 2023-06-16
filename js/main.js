2d2d2e7eaec5e53b036fb73e66c9fa99d44c1e45
marcocampisi <marco.campisi@outlook.it>
2023-06-16 16:35:39 +0200

    Added up button functionality

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@ const carouselImages = document.getElementsByClassName('carouselImage');
 const upButton = document.getElementById("upButton");
 const downButton = document.getElementById("downButton");
 
+let activeIndex = 0;
+
+upButton.addEventListener('click',
+    function() {
+        carouselImages[activeIndex].classList.remove("active");
+        activeIndex--;
+        if (activeIndex < 0) {
+            activeIndex = carouselImages.length - 1;
+        }
+        carouselImages[activeIndex].classList.add("active");
+    }
+);
